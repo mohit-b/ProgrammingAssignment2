7f3ceb0d10df4118e7d323d9469c954aa38156a3
mohit-b <mohitbadiyani123@gmail.com>
2020-05-15 21:32:17 +0530

    Final_1

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,3 @@
-##  Catching the Inverse of the Matrix
-
-## The first function, makeCacheMatrix creates a special "matrix", 
-## which is really a list containing a function to:
-##  1. set the value of the matrix
-##  2. get the value of the matrix
-##  3. set the value of the inverse
-##  4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -21,13 +13,6 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 
 }
-
-
-## This function computes the inverse of the special "matrix" 
-## returned by makeCacheMatrix above. 
-## If the inverse has already been calculated (and the matrix has not changed), 
-## then the cachesolve should retrieve the inverse from the cache.
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     m <- x$getinverse()
@@ -42,29 +27,3 @@ cacheSolve <- function(x, ...) {
 }
 
 
-
-## Test Cases:
-
-a <- makeCacheMatrix(matrix(1:4, 2, 2))
-a$get()
-cacheSolve(a)
-
-b <- makeCacheMatrix(matrix(4:8, 2, 2))
-b$get()
-cacheSolve(b)
-
-c = matrix(c(1,-0.25,-0.25,1),nrow=2,ncol=2,byrow=T)
-ci = cacheSolve(makeCacheMatrix(a))
-ci
-
-d = matrix(c(4,7,2,6),nrow=2,ncol=2,byrow=T)
-di = cacheSolve(makeCacheMatrix(a))
-di
-
-ai1 = cacheSolve(makeCacheMatrix(a))
-am = makeCacheMatrix(a)
-ai = cacheSolve(am)
-ai2 = cacheSolve(am)
-getting cached data
-ai %*% a
-a %*% ai2
